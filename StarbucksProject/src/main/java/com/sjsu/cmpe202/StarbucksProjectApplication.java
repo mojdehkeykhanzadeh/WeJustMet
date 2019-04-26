b53c4280d2c7bda7a940bddff310956375d289ae
Mojdeh Keykhanzadeh <mojdeh.keykhanzadeh@sjsu.edu>
2019-04-25 22:43:11 -0700

    added initial setup

---
+++ StarbucksProject/src/main/java/com/sjsu/cmpe202/StarbucksProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sjsu.cmpe202;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarbucksProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarbucksProjectApplication.class, args);
+	}
+
+}
