6dd1599afee1af12871c14039ced2e2a53ed533c
Sangwon Song <sangwon.song@sjsu.edu>
2019-05-04 11:45:49 -0700

    User Repository Update

--- StarbucksProject/src/main/java/com/sjsu/cmpe202/repository/UserRepository.java
+++ StarbucksProject/src/main/java/com/sjsu/cmpe202/repository/UserRepository.java
@@ -1,10 +1,15 @@
-package com.sjsu.cmpe202.repository;
+package com.example.mongo.api.repository;
+
+import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-import com.sjsu.cmpe202.model.User;
+import com.example.mongo.api.model.User;
 
-public interface UserRepository extends MongoRepository<User, String> {
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+	public User findByEmail(String email);
+	public List<User> findByFirstName (String firstName);
 
-	User findByEmail(String email);
 }
