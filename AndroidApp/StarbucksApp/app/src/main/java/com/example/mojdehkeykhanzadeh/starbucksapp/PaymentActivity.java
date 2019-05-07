09b745bd1fafe2c2bc8c2a3c8cb50457abb17d4d
Mojdeh Keykhanzadeh <mojdeh.keykhanzadeh@sjsu.edu>
2019-05-06 21:45:46 -0700

    added payment screen functionality

--- AndroidApp/StarbucksApp/app/src/main/java/com/example/mojdehkeykhanzadeh/starbucksapp/PaymentActivity.java
+++ AndroidApp/StarbucksApp/app/src/main/java/com/example/mojdehkeykhanzadeh/starbucksapp/PaymentActivity.java
@@ -1,13 +1,96 @@
 package com.example.mojdehkeykhanzadeh.starbucksapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class PaymentActivity extends AppCompatActivity {
+    private String balance;
+    private String cardNumber;
+    private Button pay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        cardNumber= bundle.getString("card number");
+        getBalance();
+        pay = findViewById(R.id.buttonPay);
+        pay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Add Card Response", Toast.LENGTH_SHORT).show();
+                makePayment();
+            }
+        });
+    }
+    public void getBalance() {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String URL = "http://10.0.2.2:8080/getBalance?userId=12345&cardNumber="+cardNumber;
+        StringRequest objectRequest = new StringRequest(Request.Method.GET, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.e("Get Response", response);
+                        balance = new String(response);
+                        TextView balanceText = (TextView) findViewById(R.id.balance);
+                        balanceText.setText(balance);
+
+                    }
+                },
+                new Response.ErrorListener()  {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Get Error Response", error.toString());
+                    }
+
+
+                });
+        requestQueue.add(objectRequest);
+
+    }
+    public void makePayment(){
+        int random = (int)(Math.random() * 50 + 1);
+        String URL ="http://10.0.2.2:8080/makePayment?id="+random+"&cardNumber=" +cardNumber+
+                "&amount= 10.00&userId= 12345";;
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        StringRequest objectRequest = new StringRequest(Request.Method.POST, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.e("Response", response);
+                        Toast toast= Toast.makeText(getApplicationContext(),
+                                response, Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 0);
+                        toast.show();
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Rest Response", error.toString());
+                    }
+                }) {
+
+        };
+        requestQueue.add(objectRequest);
     }
 }
