b1c7505df73ba2789cad2632bef71c7685e35b83
Sangwon Song <sangwon.song@sjsu.edu>
2019-05-10 06:15:52 -0700

    Updated Screen Flow

--- AndroidApp/StarbucksApp/app/src/main/java/com/example/mojdehkeykhanzadeh/starbucksapp/MainActivity.java
+++ AndroidApp/StarbucksApp/app/src/main/java/com/example/mojdehkeykhanzadeh/starbucksapp/MainActivity.java
@@ -12,35 +12,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn1 = (Button) findViewById(R.id.buttonDummy);
         Button btn2 = (Button) findViewById(R.id.buttonRegister);
-        Button btn3 = (Button) findViewById(R.id.buttonOrder);
-        btn1.setOnClickListener(this);
-        /*btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), AddCardActivity.class);
-                startActivity(intent);
-            }
-        });*/
         btn2.setOnClickListener(this);
-        btn3.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v){
         switch (v.getId()){
-            case R.id.buttonDummy:
-                Intent intent = new Intent(v.getContext(), AddCardActivity.class);
-                startActivity(intent);
-                break;
             case R.id.buttonRegister:
                 Intent intent2 = new Intent(v.getContext(), RegisterUser.class);
                 startActivity(intent2);
-                break;
-            case R.id.buttonOrder:
-                Intent intent3 = new Intent(v.getContext(), OrderActivity.class);
-                startActivity(intent3);
                 break;
             default:
                 break;
